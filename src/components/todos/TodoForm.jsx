3019c05aac70482a2662ab335bc745e85b800dc9
sohseyedi <sohseyedi@gmail.com>
2023-07-09 21:23:23 +0330

    complete todos

---
+++ src/components/todos/TodoForm.jsx
@@ -0,0 +1,54 @@
+import * as RiIcon from "react-icons/ri";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useTimerDispatch } from "../../context/TimerProvider";
+import { toast } from "react-hot-toast";
+
+const initialValues = {
+  title: "",
+};
+
+const validationSchema = Yup.object({
+  title: Yup.string().required("مقداری وارد نشده است"),
+});
+
+const TodoForm = () => {
+  const dispatch = useTimerDispatch();
+  const onSubmit = (values, { resetForm }) => {
+    dispatch({ type: "ADD_TODO", payload: values });
+    resetForm();
+    toast.success("ثبت شد");
+  };
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
+    validateOnMount: true,
+    enableReinitialize: true,
+  });
+  return (
+    <form
+      className="my-5 w-[90%] mx-auto bg-white rounded-md shadow-md h-[40px] flex"
+      onSubmit={formik.handleSubmit}
+    >
+      <input
+        type="text"
+        id="title"
+        name="title"
+        {...formik.getFieldProps("title")}
+        placeholder="برای شروع، کار جدید ثبت کن"
+        className="h-full outline-none border-none pr-2 rounded-md w-[87%]"
+      />
+      <button
+        disabled={!formik.isValid}
+        className={`${
+          !formik.isValid ? "opacity-50" : "opacity-100"
+        } bg-[#9190d9] text-white border-none rounded-l-md w-[13%] flex items-center justify-center h-full`}
+      >
+        <RiIcon.RiPencilFill size={22} />
+      </button>
+    </form>
+  );
+};
+
+export default TodoForm;
--- src/components/todos/TodoList.jsx
+++ src/components/todos/TodoList.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import TodoForm from "./TodoForm";
+import * as RiIcon from "react-icons/ri";
+import { useTimer, useTimerDispatch } from "../../context/TimerProvider";
+import { toast } from "react-hot-toast";
 
 const TodoList = () => {
-  return (
-    <div>TodoList</div>
-  )
-}
+  const { timers } = useTimer();
+  const dispatch = useTimerDispatch();
 
-export default TodoList
+  const removeItemHandler = (timer) => {
+    dispatch({ type: "REMOVE_TODO", payload: timer });
+    toast.error("حذف شد");
+  };
+
+  const activeItemHandler = (timer) => {
+    dispatch({ type: "ACTIVE_TODO", payload: timer });
+    toast.success("فعال شد");
+  };
+
+  return (
+    <section>
+      <h4 className="text-center text-white font-semibold pt-3 text-2xl">
+        یادداشت ها
+      </h4>
+      <TodoForm />
+      <ul className="my-3">
+        {timers.length === 0 ? (
+          <p className="text-center text-white">یادداشتی وجود ندارد</p>
+        ) : (
+          timers.map((time) => (
+            <li
+              key={time.id}
+              className="my-2 px-2 w-[90%] mx-auto text-white shadow-md py-2 border-2 border-[#9190d9] rounded-md flex items-center justify-between"
+            >
+              <h5 className={`text-lg font-semibold m-0 `}>{time.title}</h5>
+              <div className="flex items-center gap-x-3">
+                <span
+                  className="text-white cursor-pointer z-30 p-[6px] bg-[#9190d9] rounded-md"
+                  onClick={() => activeItemHandler(time)}
+                >
+                  <RiIcon.RiCheckLine size={21} />
+                </span>
+                <span
+                  className="text-white cursor-pointer z-30 p-[6px] bg-[#9190d9] rounded-md"
+                  onClick={() => removeItemHandler(time)}
+                >
+                  <RiIcon.RiDeleteBin5Fill size={21} />
+                </span>
+              </div>
+            </li>
+          ))
+        )}
+      </ul>
+    </section>
+  );
+};
+
+export default TodoList;
