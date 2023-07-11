f44db10c374abdfa16fdf00f7ddb5221969dd7bb
sohseyedi <sohseyedi@gmail.com>
2023-07-11 11:07:18 +0330

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { Route, Routes } from "react-router";
-import Layout from "./components/layout/Layout";
 import Setting from "./components/setting/Setting";
 import TodoList from "./components/todos/TodoList";
 import Profile from "./components/profile/Profile";
 import Navigation from "./components/common/Navigation";
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
     <div className="max-w-screen-sm md:max-w-sm mx-auto relative h-screen overflow-hidden bg-[#1e1b2e]">
       <Routes>
-        <Route path="/" element={<Layout />} />
         <Route path="/setting" element={<Setting />} />
+        <Route path="/" element={<Layout/>}/>
         <Route path="/todos" element={<TodoList />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
