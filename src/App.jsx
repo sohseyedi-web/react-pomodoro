5ebeda02403e795f50e0cd8c418084d7871c146f
sohseyedi <sohseyedi@gmail.com>
2023-07-11 12:13:21 +0330

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import TodoList from "./components/todos/TodoList";
-import Layout from "./components/layout/Layout";
+import Timer from "./components/layout/Layout";
 import Setting from "./components/setting/Setting";
 import Profile from "./components/profile/Profile";
 import Navigation from './components/common/Navigation';
@@ -10,7 +10,7 @@ function App() {
     <div className="max-w-screen-sm md:max-w-sm mx-auto relative h-screen overflow-hidden dark:bg-[#1e1b2e] bg-gray-100">
       <Routes>
         <Route path="/todos" element={<TodoList />} />
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Timer />} />
         <Route path="/setting" element={<Setting />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
