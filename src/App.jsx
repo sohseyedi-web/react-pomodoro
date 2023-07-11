41da6e1e5bb23287f8d6934ef1042523195ec6ea
sohseyedi <sohseyedi@gmail.com>
2023-07-11 11:18:38 +0330

    change router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { Route, Routes } from "react-router";
-import Setting from "./components/setting/Setting";
-import TodoList from "./components/todos/TodoList";
-import Profile from "./components/profile/Profile";
-import Navigation from "./components/common/Navigation";
-import Layout from "./components/layout/Layout";
 import { BrowserRouter } from "react-router-dom";
+import TodoList from "./components/todos/TodoList";
+import Layout from "./components/layout/Layout";
+import Setting from "./components/setting/Setting";
+import Profile from "./components/profile/Profile";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="max-w-screen-sm md:max-w-sm mx-auto relative h-screen overflow-hidden bg-[#1e1b2e]">
         <Routes>
-          <Route path="/setting" element={<Setting />} />
-          <Route path="/" element={<Layout />} />
           <Route path="/todos" element={<TodoList />} />
+          <Route path="/" element={<Layout />} />
+          <Route path="/setting" element={<Setting />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
         <Navigation />
